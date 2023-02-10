b6e23515cb4cb1c3d1bd48747512ccf3d65e0206
Daniils Petrovs <thedanpetrov@gmail.com>
2023-02-10 23:30:31 +0100

    Add new flag - `minTokenCount`

--- main.go
+++ main.go
@@ -6,14 +6,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 	"time"
 
 	astisub "github.com/asticode/go-astisub"
 )
 
+const defaultMinTokenCount = 3
+
 func main() {
 	inputFilePath := flag.String("i", "", "input file (e.g. .vtt)")
 	outputFilePath := flag.String("o", *inputFilePath+".txt", "output file (e.g. res.txt)")
+
+	minTokenCount := flag.Int("minTokenCount", defaultMinTokenCount, "minimum number of tokens (words) for a timestamp line to be registered individually")
 
 	flag.Parse()
 
@@ -21,41 +26,61 @@ func main() {
 		log.Fatal("missing required argument: inputFilePath")
 	}
 
-	doProcessing(*inputFilePath, *outputFilePath)
+	doProcessing(inputFilePath, outputFilePath, minTokenCount)
 }
 
-func doProcessing(inputFilePath, outputFilePath string) {
+func doProcessing(inputFilePath, outputFilePath *string, minTokenCount *int) {
 	start := time.Now()
 
-	fmt.Println("input file:", inputFilePath)
-	fmt.Println("output file:", outputFilePath)
+	fmt.Println("input file:", *inputFilePath)
+	fmt.Println("output file:", *outputFilePath)
 
-	data, err := os.Open(inputFilePath)
+	data, err := os.Open(*inputFilePath)
 	check(err)
 	defer data.Close()
 
 	subs, err := astisub.ReadFromWebVTT(data) // FIXME: Check actual file format
 	check(err)
 
-	outputFile, err := os.Create(outputFilePath)
+	outputFile, err := os.Create(*outputFilePath)
 	check(err)
 	defer outputFile.Close()
 
 	writer := bufio.NewWriter(outputFile)
 
-	count := 0
+	var initialCount, finalCount int
 
+	var subText, timestamp string
+
+	// Iterate over subs and write to output file
 	for _, item := range subs.Items {
-		line := formatTimestamp(item.StartAt) + " " + item.String() + "\n"
-		_, err := writer.WriteString(line)
-		check(err)
-		writer.Flush()
-		count++
+		if timestamp == "" {
+			timestamp = formatTimestamp(item.StartAt)
+		}
+
+		subText += " " + item.String()
+		tokenCount := len(strings.Fields(subText))
+
+		if tokenCount >= *minTokenCount {
+			// Dump it
+			line := timestamp + " " + subText + "\n"
+
+			_, err := writer.WriteString(line)
+			check(err)
+			writer.Flush()
+
+			timestamp = ""
+			subText = ""
+			finalCount++
+		}
+
+		initialCount++
 	}
 
 	elapsed := time.Since(start)
 
-	fmt.Printf("finished processing %d items in %f seconds\n", count, elapsed.Seconds())
+	fmt.Printf("finished processing in %f seconds\nshrank entry count from %d --> %d\n",
+		elapsed.Seconds(), initialCount, finalCount)
 }
 
 func check(err error) {
